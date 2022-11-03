abc4320dc13630ddd28ffda3c2417cf16af0efdf
Posedooon <b2200930@photon.chitose.ac.jp>
2022-11-04 00:41:18 +0900

    Exercise3_2完了

--- src/lecture02/Exercise2_2.java
+++ src/lecture02/Exercise2_2.java
@@ -2,15 +2,15 @@ package lecture02;
 
 public class Exercise2_2 {
     public static void main(String[] args) {
-    /*2-3の変更による影響でコメントアウト部分がないとエラー起こっちゃいます
-    Tire[] tires = new Tire[4];
+        /*2-3の変更による影響でここないとエラー起こっちゃいます*/
+        Tire[] tires = new Tire[4];
 
         for(int i = 0;i < tires.length;i++){
             tires[i] = new Tire(65);
         }
 
-        Engine engine = new Engine(4000);*/
-        Car car = new Car(/*tires,engine*/);
+        Engine engine = new Engine(4000);
+        Car car = new Car(tires,engine);
         car.run();
         GasStation gasStation = new GasStation();
         gasStation.refuel(car);
