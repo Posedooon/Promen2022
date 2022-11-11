fcb7322ec06e566030bf388f4dcf0a0c8b338e55
Posedooon <b2200930@photon.chitose.ac.jp>
2022-11-11 13:35:29 +0900

    Exercise4_2終了

--- src/lecture04/Exercise4_2.java
+++ src/lecture04/Exercise4_2.java
@@ -2,6 +2,15 @@ package lecture04;
 
 public class Exercise4_2 {
     public static void main(String[] args) {
+        ATM atm = new ATM();
+        atm.existsAccount("石田雪也","12345");
 
+        atm.registerAccount("石田雪也","12345");
+
+        if(atm.existsAccount("石田雪也","12345")){
+            atm.deposit("12345", 1000000);
+            atm.withdraw("12345", 200000);
+            atm.withdraw("12345", 500000);
+        }
     }
 }
